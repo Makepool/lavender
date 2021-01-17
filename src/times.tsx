f59160baaa1809793622f58529a83bcec9a5bfc9
makepool <makepool@hotmail.com>
2021-01-17 00:16:37 +0000

    Show today dynamically.

--- src/times.tsx
+++ src/times.tsx
@@ -1,38 +1,33 @@
 import './times.scss';
+import cn from 'classnames';
 
 export default function Times() {
+    const date = new Date();
+    let int = date.getDay() - 1;
+    if (int === -1) {
+      int = 6;
+    }
+
+    const days = [
+        {id: 0, day: 'Monday', times: '9am‑8pm'},
+        {id: 1, day: 'Tuesday', times: '9am‑6pm'},
+        {id: 2, day: 'Wednesday', times: '9am‑6pm'},
+        {id: 3, day: 'Thursday', times: '9am‑8pm'},
+        {id: 4, day: 'Friday', times: '9am‑6pm'},
+        {id: 5, day: 'Saturday', times: '9am‑6pm'},
+        {id: 6, day: 'Sunday', times: '9am‑6pm'}
+    ];
+
     return (
         <div className="times">
             <table id="open-times" className="times__inner">
                 <tbody>
-                    <tr>
-                        <td>Monday</td>
-                        <td>9am‑8pm</td>
-                    </tr>
-                    <tr>
-                        <td>Tuesday</td>
-                        <td>9am‑6pm</td>
-                    </tr>
-                    <tr>
-                        <td>Wednesday</td>
-                        <td>9am‑6pm</td>
-                    </tr>
-                    <tr>
-                        <td>Thursday</td>
-                        <td>9am‑8pm</td>
-                    </tr>
-                    <tr className="today">
-                        <td>Friday</td>
-                        <td>9am‑6pm</td>
-                    </tr>
-                    <tr>
-                        <td>Saturday</td>
-                        <td>9am‑6pm</td>
-                    </tr>
-                    <tr>
-                        <td>Sunday</td>
-                        <td>12am‑6pm</td>
-                    </tr>
+                    {days.map((page: any) => (
+                        <tr key={page.id} className={cn({today: int === page.id})}>
+                            <td>{page.day}</td>
+                            <td>{page.times}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
